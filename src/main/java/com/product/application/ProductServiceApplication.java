5eb84a18af4db473bfd3bd167c3daa014dc03cf5
Durgamani_Devarapu <durgamani.dev@gmail.com>
2019-02-26 14:54:35 +0530

    changed package name from ProductService to application

--- src/main/java/com/product/application/ProductServiceApplication.java
+++ src/main/java/com/product/application/ProductServiceApplication.java
@@ -1,12 +1,8 @@
-package com.product.ProductService;
+package com.product.application;
 
-import com.product.controller.ProductServiceController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
