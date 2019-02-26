798e42daf78d9acebdef07442ccb8018f887002d
Durgamani_Devarapu <durgamani.dev@gmail.com>
2019-02-26 14:43:10 +0530

    adding service impl class

--- src/main/java/com/product/service/ProductServiceImpl.java
+++ src/main/java/com/product/service/ProductServiceImpl.java
@@ -67,7 +67,6 @@ public class ProductServiceImpl  implements ProductService{
     }
 
 
-
     @Override
     public void deleteProduct(long id) {
         log.info("deleting product with id:{}",id);
