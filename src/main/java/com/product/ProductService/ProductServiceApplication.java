bec2a96a3cba045361775ae401c37e90fd482c3c
Durgamani_Devarapu <durgamani.dev@gmail.com>
2019-02-26 14:48:56 +0530

    added componentscan in application

--- src/main/java/com/product/ProductService/ProductServiceApplication.java
+++ src/main/java/com/product/ProductService/ProductServiceApplication.java
@@ -1,11 +1,18 @@
 package com.product.ProductService;
 
+import com.product.controller.ProductServiceController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.product.*")
+@EnableAutoConfiguration
 public class ProductServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
