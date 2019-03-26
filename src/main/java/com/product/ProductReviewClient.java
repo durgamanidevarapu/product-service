5f307ef1d0473119760d810026150e27fb29fef1
Durgamani_Devarapu <durgamani.dev@gmail.com>
2019-03-26 14:53:40 +0530

    adding feign client

---
+++ src/main/java/com/product/ProductReviewClient.java
@@ -0,0 +1,23 @@
+package com.product;
+
+
+import com.product.domain.ReviewDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name="REVIEW-SERVICE")
+public interface ProductReviewClient {
+
+    @GetMapping(value="/{productId}/reviews")
+    List<ReviewDto> getProductReviews(@PathVariable(value="productId") Long productId);
+
+    @PostMapping(value="/products/{productId}/reviews")
+    ResponseEntity<Object> addProductReview(@RequestBody ReviewDto reviewDto, @PathVariable(value="productId") Long productId);
+
+}
--- src/main/java/com/product/ProductServiceApplication.java
+++ src/main/java/com/product/ProductServiceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+@EnableFeignClients
 public class ProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
