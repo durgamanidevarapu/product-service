204ac86531862203eacba6d72d6b649ec701c2b1
Durgamani_Devarapu <durgamani.dev@gmail.com>
2019-03-26 14:56:47 +0530

    removing restemplate and eureka client from service

--- src/main/java/com/product/service/ProductReviewServiceImpl.java
+++ src/main/java/com/product/service/ProductReviewServiceImpl.java
@@ -1,23 +1,15 @@
 package com.product.service;
 
-import com.netflix.discovery.EurekaClient;
 import com.product.ProductReviewClient;
 import com.product.domain.ReviewDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service(value="productReviewService")
 public class ProductReviewServiceImpl implements ProductReviewService {
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @Autowired
-    EurekaClient eurekaClient;
 
     @Autowired
     ProductReviewClient productReviewClient;
