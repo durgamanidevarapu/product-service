1a47191e343aea7474ff29cef78d90af40edcffe
Durgamani_Devarapu <durgamani.dev@gmail.com>
2019-03-28 12:10:09 +0530

    integrating  hystrix

--- src/main/java/com/product/ProductReviewFallback.java
+++ src/main/java/com/product/ProductReviewFallback.java
@@ -1,11 +1,14 @@
 package com.product;
 
 import com.product.domain.ReviewDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 
+@Component
 public class ProductReviewFallback implements  ProductReviewClient {
     @Override
     public List<ReviewDto> getProductReviews(Long productId) {
@@ -14,6 +17,6 @@ public class ProductReviewFallback implements  ProductReviewClient {
 
     @Override
     public ResponseEntity<Object> addProductReview(ReviewDto reviewDto, Long productId) {
-        return null;
+        return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
     }
 }
